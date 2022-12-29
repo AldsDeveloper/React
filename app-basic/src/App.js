93dc884a365baedb5b9a5ad835efc6b421b8e092
Abdulloh404 <adlforgithub@gmail.com>
2022-12-29 16:34:36 +0700

    Update App.js

--- app-basic/src/App.js
+++ app-basic/src/App.js
@@ -5,16 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>Hello world reactjs</h1>
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
