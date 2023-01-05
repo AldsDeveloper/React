82928c9b1cc73418defdd7236841affd24b22208
Abdulloh404 <adlforgithub@gmail.com>
2023-01-05 11:06:36 +0700

    Update App.js

--- app-basic/src/App.js
+++ app-basic/src/App.js
@@ -2,71 +2,67 @@ import React from 'react';
 import './index.css';
 
 function App() {
-  /*
-  return (
+  /*return (
     <h3>
       {' '}
       Hello World! <br></br>
       <h1>สวัสดีชาวโลกฉันมาแล้ว</h1>{' '}
     </h3>
-  );
-*/
-
-  let r = 'React';
-  let rn = 'React Native';
-  const el = (
-    <h1>
-      <center>
-        Create Web & Mobile Apps
-        <br />
-        with {r} & {rn}
-      </center>
-    </h1>
-  );
-  return el;
-
-  /*
-	return (
-		<table border="1">
-			<tr><th>Product</th><th>Price</th></tr>
-			<tr><td>React</td><td>320</td></tr>
-			<tr><td>React Native</td><td>340</td></tr>
-		</table>
-	)
-	*/
-  /*
-	const divStyle = {
+  );*/
+  /*let r = 'React';
+    let rn = 'React Native';
+    const el = (
+      <h1>
+        <center>
+          Create Web & Mobile Apps
+          <br />
+          with {r} & {rn}
+        </center>
+      </h1>
+    );
+    return el;*/
+  /*return (
+      <table border="1">
+        <tr>
+          <th>Product</th>
+          <th>Price</th>
+        </tr>
+        <tr>
+          <td>React</td>
+          <td>320</td>
+        </tr>
+        <tr>
+          <td>React Native</td>
+          <td>340</td>
+        </tr>
+      </table>
+    );*/
+  /*const divStyle = {
 		color: 'red',
 		backgroundColor: 'powderblue',		
 		fontSize: 'larger',
 		padding: '3px',
 	}
-	return <div style={divStyle}>Hello React</div>
-	*/
-  /*
-	let colors = ['red', 'green', 'blue', 'yellow']
+	return <div style={divStyle}>Hello React</div>*/
+  /*let colors = ['red', 'green', 'blue', 'yellow']
 	let list = colors.map(c => <li>{c}</li>)		//แทรกค่าแบบ JSX
-	return <ul>{list}</ul>
-	*/
-  /*
-	let colors = ['red', 'green', 'blue', 'yellow']
+	return <ul>{list}</ul>*/
+  /*let colors = ['red', 'green', 'blue', 'yellow']
 	return (
 		<ul>
 			{colors.map(c => <li>{c}</li>)}
 		</ul> 
-	)
-	*/
-  /*
-	return (
+	)*/
+  /*return (
 		<ul>
 			{colors.map(c => <li>{c}</li>)}
 		</ul>
 	)
-	*/
-  //return <img src="./logo192.png" alt=""/>
-  //return <img src={"./logo192.png"} alt=""/>
-  //return <img src={(process.env.PUBLIC_URL + "/logo192.png")} alt=""/>
-  //return <img src={reactlogo} width="10%" alt=""/>
+	
+	return <img src="./logo192.png" alt=""/>
+	return <img src={"./logo192.png"} alt=""/>
+	return <img src={(process.env.PUBLIC_URL + "/logo192.png")} alt=""/>
+	return <img src={reactlogo} width="10%" alt=""/>*/
 }
 
 export default App;
